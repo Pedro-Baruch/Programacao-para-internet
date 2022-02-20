9cc1c346188bc9a34be97de27fae8e866edea04b
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2022-02-20 01:20:03 -0300

    resolução

---
+++ Atividades/Atividade01/q04.js
@@ -0,0 +1,15 @@
+const axios = require('axios').default
+const cheerio = require('cheerio').default
+const input = require('prompt-sync')()
+
+function main(){
+    const url = 'https://github.com/'
+
+    links(url)
+}
+
+async function links(url){
+
+}
+
+main()
