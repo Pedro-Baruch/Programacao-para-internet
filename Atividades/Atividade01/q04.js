f5f79952f824c959677831b85b0b355e0f0798ab
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2022-02-20 12:58:03 -0300

    Update q04.js

--- Atividades/Atividade01/q04.js
+++ Atividades/Atividade01/q04.js
@@ -2,7 +2,7 @@ const axios = require('axios').default
 const cheerio = require('cheerio').default
 
 function main(){
-    const url = 'https://ifpi.edu.br/teresinacentral'
+    const url = 'https://github.com/'
 
     scraping(url)
 }
@@ -11,28 +11,10 @@ async function scraping(url){
     await axios.get(url)
         .then(response => {
             const $ = cheerio.load(response.data)
-            
-            console.log("-----------NAVTREE-----------")
-            $('.navTreeItem').each((index,element) => {
-                const link = $(element).find('a').attr("href")
-                
+            $('a').each((index,element) => {
+                const link = $(element).attr("href")
                 console.log(link)
             })
-
-            console.log('--------------ROW-------------')
-            $('.row').each((index,element) => {
-                const link = $(element).find('a').attr("href")
-
-                console.log(link)
-            })
-
-            console.log('------PORTALSERVICOS------------')
-            $('.portalservicos-item').each((index,element) => {
-                const link = $(element).find('a').attr("href")
-
-                console.log(link)
-            })
-
         }).catch(error => {
             console.error(error)
         })
